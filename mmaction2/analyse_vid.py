f4edf8be8aab32a021e5d34e943dc0a3212df68e
atou0004 <114662475+atou0004@users.noreply.github.com>
2023-05-14 06:35:28 +0800

    Added mmaction2 files part 3

---
+++ mmaction2/analyse_vid.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+import numpy as np
+import argparse
+import re
+
+def human_action_recognition(input_video):
+    # Analysing one vid at a time
+    # Path to the label map file
+    label_map_file = 'tools/data/kinetics/label_map_k600.txt'
+
+    # Set up the command for running action recognition
+    # uniformerv2 human action recognition model
+    #the_cmd = 'python demo/demo_inferencer.py {} --print-result --rec configs/recognition/uniformerv2/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb.py --rec-weights checkpoints/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb_20221219-f984f5d2.pth --label-file {}'
+    # for cpu users, use the code below and comment the code above
+    the_cmd = 'python demo/demo_inferencer.py {} --print-result --device cpu --rec configs/recognition/uniformerv2/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb.py --rec-weights checkpoints/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb_20221219-f984f5d2.pth --label-file {}'
+
+    # Get the input folder path
+    input_folder = os.path.dirname(input_video)
+
+    # Set up the command to run action recognition on the input video
+    command = the_cmd.format(input_video, label_map_file)
+
+    # Run the command
+    output = subprocess.run(command, shell=True, capture_output=True)
+    # Convert the standard output of the command into a readable string
+    output_str = output.stdout.decode('utf-8').replace(command, '')
+
+    # Regular expression pattern to match the dict
+    pattern = r"\{.*\}"
+
+    # extract dictionary using regular expression
+    match = re.search(pattern, output_str)
+    if match:
+        result_dict = eval(match.group())
+    
+    # get top5 labels
+    # converting the dict to string so that it can be split in the get_top5_labels function
+    return get_top5_labels(str(result_dict), label_map_file)
+
+
+def get_top5_labels (the_dict, label_map_file):
+    # Takes the output pred dict and label file as input
+    # Extract the scores from the output string
+    scores_str = the_dict.split('[')[-1].split(']')[0]
+    scores = np.fromiter(scores_str.split(','), dtype=np.float32)
+
+    # Load the label map file
+    with open(label_map_file, 'r') as f:
+        labels = f.read().splitlines()
+
+    # Get the indices that would sort the scores in descending order
+    sorted_indices = np.argsort(scores)[::-1]
+
+    # Get the top 5 actions and their scores
+    top5_indices = sorted_indices[:5]
+
+    # Get the labels for the top 5 actions
+    top5_labels = [labels[idx] for idx in top5_indices]
+
+    return top5_labels
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Human Action Recognition')
+    parser.add_argument('input_file', help='input video file')
+    args = parser.parse_args()
+
+    # Call the action recognition function with the input video file
+    # Only takes one video as input
+    print(human_action_recognition(args.input_file))
---
+++ mmaction2/analyse_vid2.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+import subprocess
+import json
+import numpy as np
+
+
+def get_both_vid_and_top5labels(input_video):
+    # Analysing one vid at a time
+    # Path to the label map file
+    label_map_file = 'tools/data/kinetics/label_map_k600.txt'
+
+    # Set up the command for running action recognition
+    #the_cmd = 'python demo/demo_inferencer.py {} --vid-out-dir {} --pred-out-file {} --rec configs/recognition/uniformerv2/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb.py --rec-weights checkpoints/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb_20221219-f984f5d2.pth --label-file {}'
+    # for cpu users, use the code below and comment the code above
+    the_cmd = 'python demo/demo_inferencer.py {} --vid-out-dir {} --pred-out-file {} --device cpu --rec configs/recognition/uniformerv2/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb.py --rec-weights checkpoints/uniformerv2-large-p14-res336_clip-kinetics710-pre_u32_kinetics600-rgb_20221219-f984f5d2.pth --label-file {}'
+    
+    # Set up the input and output file paths
+    video_path = input_video
+    output_folder = os.path.join(os.path.dirname(video_path), 'results')
+    pred_out_file = os.path.join(output_folder, os.path.splitext(os.path.basename(video_path))[0] + '.json')
+
+    # Run action recognition on the input video
+    command = the_cmd.format(video_path, output_folder, pred_out_file, label_map_file)
+
+    # Run the command
+    subprocess.run(command, shell=True)
+
+    # Load the recognition results for this video
+    with open(pred_out_file) as f:
+        data = json.load(f)
+
+    scores = np.array(data['predictions'][0]['rec_scores'][0])
+    sorted_indices = np.argsort(scores)[::-1]
+
+    # Get the top 5 actions
+    top5_indices = sorted_indices[:5]
+    top5_actions = [str(idx) for idx in top5_indices]
+
+    # Load the label map
+    with open(label_map_file, 'r') as f:
+        labels = f.read().splitlines()
+
+    # Get the labels for the top 5 actions
+    top5_labels = [labels[idx] for idx in top5_indices]
+
+    # Write the list to the output file
+    with open(pred_out_file, 'w') as f:
+        json.dump(top5_labels, f)
+    
+    return(top5_labels)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Human Action Recognition')
+    parser.add_argument('input_file', help='input video file')
+    args = parser.parse_args()
+
+    # Call the action recognition function with the input video file
+    # Only takes one video as input
+    get_both_vid_and_top5labels(args.input_file)
